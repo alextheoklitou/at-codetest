e7731eb87904d361698d80e29c26659e103d6f88
Alex Theoklitou <alex_theo_cy@hotmail.com>
2023-02-10 19:24:11 +0200

    loading feature added and able to search by clicking enter and pressing the button

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -6,6 +6,7 @@ function SearchResults() {
   const [searchResults, setSearchResults] = useState(null)
   const [searchValue, setSearchValue] = useState(null)
   const [input, setInput] = useState(null)
+  const isLoading = searchValue && !searchResults
 
 
   const handleType = (e) => {
@@ -13,6 +14,7 @@ function SearchResults() {
   }
 
   const sendRequest = (e) => {
+    e.preventDefault()
     setSearchValue(input)
   }
 
@@ -35,17 +37,22 @@ function SearchResults() {
     <>
       <div className='w-full m-10 flex flex-col items-center'>
         <div>
-          <div className="flex flex-wrap w-full mb-4">
+          <form className="flex flex-wrap w-full mb-4" onSubmit={sendRequest}>
             <input className='flex-auto px-3 py-1.5 text-gray-700 bg-white border border-solid border-gray-300 rounded m-0 focus:text-gray-700 focus:border-blue-600 focus:outline-none' placeholder='Search...' onChange={handleType} />
-            <button className='bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 ml-4 rounded uppercase' onClick={sendRequest}>Search</button>
-          </div>
+            <button className='bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 ml-4 rounded uppercase'>Search</button>
+          </form>
+          {isLoading && (
+            <h1>Just a minute while we find the best deals for you! Your next holiday is just around the corner.</h1>
+          )}
           {searchResults && searchValue && (
             <div className="relative overflow-x-auto">
               <table className="w-full text-sm text-left text-gray-500">
                 <thead className="text-S text-gray-700 uppercase bg-gray-50 border">
-                  <th scope="col" className="px-6 py-3 border">Image</th>
-                  <th scope="col" className="px-6 py-3 border">Title</th>
-                  <th scope="col" className="px-6 py-3 border">Destination</th>
+                  <tr>
+                    <th scope="col" className="px-6 py-3 border">Image</th>
+                    <th scope="col" className="px-6 py-3 border">Title</th>
+                    <th scope="col" className="px-6 py-3 border">Destination</th>
+                  </tr>
                 </thead>
                 <tbody>
                   {searchResults.map(result =>
